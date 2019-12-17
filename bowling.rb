0a40018b9da20e3ccb702815bb1e271549c8d159
Ryan Ketchum <ryan@Ryans-MacBook-Pro.local>
2019-12-16 17:43:51 -0800

    This is another attemp at the project

--- bowling.rb
+++ bowling.rb
@@ -1,4 +1,16 @@
 class Bowling
-    def hit(hit_pins)
+    attr_accessor :score
+
+    def initialize 
+        @score = 0
     end
+
+    def roll(pins)
+        @score += pins
+    end
+
+    def strike
+        if @score = "x"
+            @score + 10
+        end
 end
--- spec.rb
+++ spec.rb
@@ -2,11 +2,17 @@ require "rspec"
 require "./bowling.rb"
 
 RSpec.describe Bowling, "#score" do 
- context "game with no strikes or spares" do
-    it "sums the pin count for each roll" do  
+    it "sums the hit pins for each roll" do  
         bowling = Bowling.new
-        20.times { bowling.hit(4)}
-        expect(bowling.score).to eq 80
+        20.times { bowling.roll(5)}
+        expect(bowling.score).to eq 100
     end
 end
+
+describe Bowling, "#strike" do 
+    it "the first scored roll is X" do
+        strike = Bowling.new
+        strike.roll("x")
+        expect(strike.score).to eq += 10
+    end
 end
